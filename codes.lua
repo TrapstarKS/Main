56c24abce08907db95c1219334178cafe636dd7e
TrapstarKSSKSKSKKS <101758758+TrapstarKSSKSKSKKS@users.noreply.github.com>
2023-04-12 16:23:12 -0300

    Create codes.lua

---
+++ codes.lua
@@ -0,0 +1,32 @@
+-- codes.lua
+
+getgenv().codes = {
+    "QUESTFIX",
+    "HOLLOW",
+    "subtomaokuma",
+    "SubToKelvingts",
+    "SubToBlamspot",
+    "TOADBOIGAMING",
+    "KingLuffy",
+    "noclypso",
+    "subtosnowrbx",
+    "Cxrsed",
+    "FictioNTheFirst",
+}
+
+getgenv().codesbf = {
+    "Sub2Fer999",
+    "Enyu_is_Pro",
+    "Magicbus",
+    "JCWK",
+    "Starcodeheo",
+    "Bluxxy",
+    "SUB2GAMERROBOT_EXP1",
+    "Sub2NoobMaster123",
+    "Sub2Daigrock",
+    "Axiore",
+    "TantaiGaming",
+    "StrawHatMaine",
+    "Sub2OfficialNoobie",
+    "TheGreatAce"
+}
