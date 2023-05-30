ad02947bc9923cc9f921b039881d3eb403a42715
TrapstarKSSKSKSKKS <101758758+TrapstarKSSKSKSKKS@users.noreply.github.com>
2023-05-30 12:00:59 -0300

    Update TrapHub.lua

--- TrapHub.lua
+++ TrapHub.lua
@@ -1,4 +1,4 @@
-local repo = 'https://raw.githubusercontent.com/TrapstarKSSKSKSKKS/Main/main/'
+local repo = "https://raw.githubusercontent.com/TrapstarKSSKSKSKKS/Main/main/"
 local function LoadScript(ScriptName)
 	pcall(function()
 		t = 0
@@ -16,13 +16,21 @@ local function LoadScript(ScriptName)
 		until getgenv().Executed or t >= 30
 	end)
 end
-
-if game.PlaceId == 8304191830 or game.PlaceId == 8349889591 then -- Anime Adventures
-	LoadScript('Anime%20Adventures.lua')
-elseif game.PlaceId == 914010731 then -- RoGhoul
-	LoadScript('RoGhoul.lua')
-elseif game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then -- Blox Fruits
-	LoadScript('BloxFruits.lua')
+local Id = game.PlaceId
+local PlaceIds = {
+	["AA"] = { 8304191830, 8349889591 },
+	["RG"] = { 914010731 },
+	["BF"] = { 2753915549, 4442272183, 7449423635 },
+	["PJS"] = { 6152116144, 5956785391, 11468159863, 11468075017, 9321822839 },
+}
+if table.find(PlaceIds["AA"], Id) then -- Anime Adventures
+	LoadScript("Anime%20Adventures.lua")
+elseif table.find(PlaceIds["RG"], Id) then -- RoGhoul
+	LoadScript("RoGhoul.lua")
+elseif table.find(PlaceIds["BF"], Id) then -- Blox Fruits
+	LoadScript("BloxFruits.lua")
+elseif table.find(PlaceIds["PJS"], Id) then -- Project Slayer
+	LoadScript("PJS.lua")
 else -- Astd
-	LoadScript('Astd.lua')
+	LoadScript("Astd.lua")
 end
